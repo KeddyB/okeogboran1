4096337dbce1b249f7a9a4f0d7376231e4f25089
KeddyB <bokelusi@gmail.com>
2025-02-01 13:52:07 +0100

    commit

--- components/auth-header.tsx
+++ components/auth-header.tsx
@@ -9,25 +9,25 @@ export function AuthHeader() {
   const { data: session } = useSession()
 
   return (
-    <header className="bg-primary text-primary-foreground p-4 flex justify-between items-center">
+    <header className="bg-primary text-primary-foreground p-2 flex justify-between items-center">
       <Link href="/" className="flex items-center space-x-2">
-        <span className="text-2xl font-bold">Okeogboran</span>
+        <span className="text-xl font-bold md:text-2xl">Okeogboran</span>
       </Link>
       <div className="flex items-center space-x-4">
         {session && (
           <>
             <Link href="/advertisement">
               <Button>
-                <Megaphone className="h-5 w-5" />
+                <Megaphone className="h-3 w-3 md:h-5 md:w-5" />
               </Button>
             </Link>
             <Link href="/account">
               <Button>
-                <User className="h-5 w-5" />
+                <User className="h-3 w-3 md:h-5 md:w-5" />
               </Button>
             </Link>
             <Button onClick={() => signOut()} variant="outline">
-              <LogOut className="h-5 w-5" />
+              <LogOut className="h-3 w-3 md:h-5 md:w-5" />
             </Button>
           </>
         )}
